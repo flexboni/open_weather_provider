fe69af0ae26615762af6ea278fc49285b11bf802
boni <kgb8594@gmail.com>
2022-08-22 14:48:16 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:open_weather_provider/pages/home_page.dart';
+import 'package:open_weather_provider/providers/weather/weather_provider.dart';
+import 'package:open_weather_provider/repositories/weather_repository.dart';
+import 'package:open_weather_provider/services/weather_api_services.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<WeatherRepository>(
+          create: (context) => WeatherRepository(
+            weatherApiServices: WeatherApiServices(
+              httpClient: http.Client(),
+            ),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => WeatherProvider(
+            weatherRepository: context.read<WeatherRepository>(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Weather',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:open_weather_provider/pages/search_page.dart';
+import 'package:open_weather_provider/providers/weather/weather_provider.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? _city;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              _city = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return SearchPage();
+                }),
+              );
+              if (_city != null) {
+                context.read<WeatherProvider>().fetchWeather(_city!);
+              }
+            },
+            icon: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
