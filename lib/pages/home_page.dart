a4d1df3ec7647d74a675ad93fd38b470cf906f7c
boni <kgb8594@gmail.com>
2022-08-22 15:11:54 +0900

    HomePage widget 1 - initial, loading, error

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,6 +12,68 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String? _city;
+  late WeatherProvider _weatherProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherProvider = context.read<WeatherProvider>();
+    _weatherProvider.addListener(_registerListener);
+  }
+
+  @override
+  void dispose() {
+    _weatherProvider.removeListener(_registerListener);
+    super.dispose();
+  }
+
+  void _registerListener() {
+    final WeatherState ws = context.read<WeatherProvider>().state;
+    if (ws.status == WeatherStatus.error) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(ws.error.errMsg!),
+          );
+        },
+      );
+    }
+  }
+
+  Widget _showWeather() {
+    final state = context.watch<WeatherProvider>().state;
+    if (state.status == WeatherStatus.initial) {
+      return Center(
+        child: Text(
+          'Select a city',
+          style: const TextStyle(fontSize: 20.0),
+        ),
+      );
+    }
+
+    if (state.status == WeatherStatus.loading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (state.status == WeatherStatus.error && state.weather.name == '') {
+      return Center(
+        child: Text(
+          'Select a city',
+          style: const TextStyle(fontSize: 20.0),
+        ),
+      );
+    }
+
+    return Center(
+      child: Text(
+        state.weather.name,
+        style: TextStyle(fontSize: 18.0),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,9 +97,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Center(
-        child: Text('Home'),
-      ),
+      body: _showWeather(),
     );
   }
 }
